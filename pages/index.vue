73d59a950787c8950d97c6a4a755de1cd4770ab1
Eko Saputra <e.poetra92@gmail.com>
2022-09-16 16:00:10 +0800

    feat(homepage): integrate heroslider component

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,36 @@
 <template>
-  <Tutorial />
+  <Heroslider :data-sliders="dataSliders" />
 </template>
 
 <script>
+import Heroslider from '~/components/homepage/heroslider'
+import homepageServices from '~/core/services/homepage'
+
 export default {
   name: 'IndexPage',
+  components: { Heroslider },
+  data() {
+    return {
+      dataSliders: [],
+    }
+  },
+  mounted() {
+    this.getHeroSlider()
+  },
+  computed: {
+    getSlider() {
+      return this.dataSliders
+    },
+  },
+  methods: {
+    getHeroSlider: async function () {
+      try {
+        const { model } = await homepageServices.getHeroSlider()
+        this.dataSliders = model.dataSlider
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
 }
 </script>
