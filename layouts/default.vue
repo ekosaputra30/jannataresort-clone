1f31851ce573846f929f53180d2bf8b76cd41856
Eko Saputra <e.poetra92@gmail.com>
2022-09-16 15:57:03 +0800

    feat(setup): default layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="main">
+    <nuxt />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Default',
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/styles/global.scss';
+</style>
