1cf4d1699beae0d2523cfdd77932f0b2cb037810
Eko Saputra <e.poetra92@gmail.com>
2022-09-16 17:47:13 +0800

    feat(layout): update layout roo telement

---
+++ components/homepage/the-resort/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section section__theresort">
+    <div class="container">
+      <div class="row">
+        <div class="w-12"></div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'TheResort',
+  props: ['dataResort'],
+  data() {
+    return {}
+  },
+}
+</script>
