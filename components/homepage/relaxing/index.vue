1814935671110540bfe0230be188451b4c34a730
Eko Saputra <e.poetra92@gmail.com>
2022-09-17 20:14:07 +0800

    feat(relaxing): initial component

---
+++ components/homepage/relaxing/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="section section__relaxing"></section>
+</template>
+<script>
+export default {
+  name: 'TheRelaxing',
+}
+</script>
+<style lang="scss">
+@import '~assets/styles/mixins.scss';
+</style>
