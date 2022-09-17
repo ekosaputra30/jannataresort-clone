056401039b73c73e635c7248fc4fb40dab979b2c
Eko Saputra <e.poetra92@gmail.com>
2022-09-17 22:05:48 +0800

    feat(footer): initial reservation component

---
+++ components/footer/reservation/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="section section--bgimage section__reservation"></section>
+</template>
+<script>
+export default {
+  name: 'Reservation',
+  data() {
+    return {}
+  },
+}
+</script>
+<style lang="scss"></style>
