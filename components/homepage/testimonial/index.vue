6f586831a33404a06f5b6fec70a5e07123fda529
Eko Saputra <e.poetra92@gmail.com>
2022-09-17 20:50:19 +0800

    feat(testi): initial component

---
+++ components/homepage/testimonial/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section section__testi"></section>
+</template>
+<script>
+export default {
+  name: 'Testimonial',
+  data() {
+    return {}
+  },
+}
+</script>
+<style lang="scss">
+@import '~assets/styles/mixins.scss';
+</style>
