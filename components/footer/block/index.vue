919bcb24ed1960648633dd88d3f9532ce8daafdd
Eko Saputra <e.poetra92@gmail.com>
2022-09-17 22:06:07 +0800

    feat(footer): initial block footer component

---
+++ components/footer/block/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="section section__footerblock"></section>
+</template>
+<script>
+export default {
+  name: 'BlockFooter',
+  data() {
+    return {}
+  },
+}
+</script>
+<style lang="scss"></style>
