160d657c7e5c30701f1e5ae03ca6ec9dbf22fd1e
Eko Saputra <e.poetra92@gmail.com>
2022-09-17 14:29:18 +0800

    feat(thestay): initial component

---
+++ components/homepage/the-stay/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section section__thestay"></section>
+</template>
+<script>
+export default {
+  name: 'TheStay',
+  props: ['datastay'],
+  data() {
+    return {}
+  },
+}
+</script>
+<style lang="scss">
+.section__thestory {
+  background-color: #5f5c68;
+}
+</style>
